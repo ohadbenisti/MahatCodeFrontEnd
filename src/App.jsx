83829a588cd4892326e346df9e5087add8d1e376
YishaiMic <frylich@gmail.com>
2024-05-15 13:15:03 +0300

    modified app file, added folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-import { useState } from 'react'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./features/Header";
+import Problem from "./pages/Problem/Problem";
+import Courses from "./pages/Courses/Courses";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
 
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+      <div>
+        <Header />
+        <Route path="/">
+          {isLoggedIn ? (
+            <Content onLogout={handleLogout} />
+          ) : (
+            <Login onLogin={handleLogin} /> // Passes handleLogin function as prop to Login component
+          )}
+        </Route>
+        <Route path="/problem" component={Problem} />
+        <Route path="/courses" component={Courses} />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
