08d2d882931cdcf70363c8ff07510b61e65fd237
ohadbenisti <ohadbenisti@gmail.com>
2024-05-15 16:18:50 +0300

    fixing the basic login

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,32 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Header from "./features/Header";
-import Problem from "./pages/Problem/Problem";
-import Courses from "./pages/Courses/Courses";
+import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Login from "./pages/Login"
+// import Header from "./features/Header";
+// import Problem from "./pages/Problem/Problem";
+// import Courses from "./pages/Courses/Courses";
 import "./App.css";
 
-function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-  };
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // const handleLogin = () => {
+  //   setIsLoggedIn(true);
+  // };
 
-  const handleLogout = () => {
-    setIsLoggedIn(false);
-  };
+  // const handleLogout = () => {
+  //   setIsLoggedIn(false);
+  // };
+
+
+  
+  function App() {
 
   return (
-    <Router>
-      <div>
-        <Header />
-        <Route path="/">
-          {isLoggedIn ? (
-            <Content onLogout={handleLogout} />
-          ) : (
-            <Login onLogin={handleLogin} /> // Passes handleLogin function as prop to Login component
-          )}
-        </Route>
-        <Route path="/problem" component={Problem} />
-        <Route path="/courses" component={Courses} />
-      </div>
-    </Router>
-  );
+    <>
+      <Routes>
+        <Route path={'/'} element={<Login />} />
+      </Routes>
+    </>
+  )
 }
 
 export default App;
