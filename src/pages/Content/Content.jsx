4dd77ff25f9cf9b6a15ca144013cb2cf07388c4d
ohadbenisti <ohadbenisti@gmail.com>
2024-05-16 17:50:08 +0300

    added course component

--- src/pages/Content/Content.jsx
+++ src/pages/Content/Content.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import CoursesComponent from "../../features/Courses";
 
 const Content = ({ onLogout }) => {
   return (
     <div>
       <h1>Hello</h1>
       <button onClick={onLogout}>Logout</button>
+      <CoursesComponent />
     </div>
   );
 };
