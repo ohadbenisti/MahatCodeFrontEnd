e011fc1163ec68d6bc53aa73b61b42d77c29b6ea
Sassi1000 <michaln250@gmail.com>
2024-05-22 13:11:22 +0300

    fix-css

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -46,31 +46,34 @@ const Login = ({ onLogin }) => {
 
   ;
   return (
-    <div className="d-flex justify-content-center">
+    <div className="d-flex justify-content-center w-100 p-4">
       <nav>
         <Link to="/signup">
-          <button style={{ backgroundColor: 'lightblue', color: 'white', border: 'none', padding: '10px 100px', borderRadius: '5px', cursor: 'pointer' }}>Sign Up</button>
+          <button style={{ backgroundColor: 'lightblue', color: 'white', border: 'none', padding: '10px 72px', borderRadius: '5px', cursor: 'pointer' }}>Sign Up</button>
         </Link>
-      </nav>  
-      <form className="col-4" onSubmit={handleLogin}>
-        <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onChange={(e) => setEmail(e.target.value)} value={email} />
-          <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-          <input type="password" className="form-control" id="exampleInputPassword1" onChange={(e) => setPassword(e.target.value)} value={password} />
-        </div>
-        <div className="mb-3 form-check">
-          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-          <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" className="btn btn-primary btn-md d-block mx-auto">Submit</button>
-      </form>
+      </nav>
+      <div className="d-flex justify-content-center flex-grow-1" style={{marginLeft: '150px'}}>
+        <form className="col-4" onSubmit={handleLogin}>
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onChange={(e) => setEmail(e.target.value)} value={email} />
+            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+            <input type="password" className="form-control" id="exampleInputPassword1" onChange={(e) => setPassword(e.target.value)} value={password} />
+          </div>
+          <div className="mb-3 form-check">
+            <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+            <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+          </div>
+          <button type="submit" className="btn btn-primary btn-md d-block mx-auto">Submit</button>
+        </form>
+      </div>
     </div>
   );
 };
 
 export default Login;
+
 
