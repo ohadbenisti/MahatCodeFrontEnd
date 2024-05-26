9b604f0d4264b4023c52de9e9d53b69a7ee62918
YishaiMic <frylich@gmail.com>
2024-05-26 14:30:29 +0300

    added type effect

---
+++ src/pages/Content/TypeEffect.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import "./TypeEffect.css";
+
+const TypeEffect = () => {
+  return (
+    <div className="typewriter">
+      <h1>ללמוד, לקודד, להצליח.</h1>
+    </div>
+  );
+};
+
+export default TypeEffect;
