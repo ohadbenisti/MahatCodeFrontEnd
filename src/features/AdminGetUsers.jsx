c3cb880060d0bc5702832b7bc84d93659228bf3b
YishaiMic <frylich@gmail.com>
2024-06-02 00:01:11 +0300

    delete user function & modal

--- src/features/AdminGetUsers.jsx
+++ src/features/AdminGetUsers.jsx
@@ -1,62 +1,63 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import AlertDialog from "./AlertDialog";
+import LoadingAnimation from "./LoadingAnimation";
 
 const AdminGetUsers = ({ showUsers }) => {
-    const [users, setUsers] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        if (showUsers) {
-            const fetchUsers = async () => {
-                setLoading(true);
-                setError(null);
-                try {
-                    const url = `${import.meta.env.VITE_SERVER}/admin/users`;
-                    console.log(`Fetching users from: ${url}`);
-                    const response = await axios.get(url, { withCredentials: true });
-                    console.log('Users fetched successfully:', response.data.users);
-                    setUsers(response.data.users);
-                } catch (error) {
-                    console.error('Error fetching users:', error.response ? error.response.data : error.message);
-                    setError('Failed to fetch users');
-                } finally {
-                    setLoading(false);
-                }
-            };
-
-            fetchUsers();
-        }
-    }, [showUsers]);
-
-    if (!showUsers) {
-        return null;
+  const fetchUsers = async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      const url = `${import.meta.env.VITE_SERVER}/admin/users`;
+      const response = await axios.get(url, { withCredentials: true });
+      setUsers(response.data.users);
+    } catch (error) {
+      console.error(
+        "Error fetching users:",
+        error.response ? error.response.data : error.message
+      );
+      setError("Failed to fetch users");
+    } finally {
+      setLoading(false);
     }
+  };
 
-    return (
-        <div>
-            {loading && <div>Loading...</div>}
-            {error && <div>{error}</div>}
-            {!loading && !error && (
-                <table className="table table-striped table-hover">
-                    <thead>
-                        <tr>
-                            <th>שם משתמש</th>
-                            <th>מייל</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {users.map(user => (
-                            <tr key={user.id}>
-                                <td>{user.name}</td>
-                                <td>{user.email}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            )}
-        </div>
-    );
+  useEffect(() => {
+    if (showUsers) fetchUsers();
+    if (!showUsers) return null;
+  }, [showUsers]);
+
+  return (
+    <div>
+      {loading && <LoadingAnimation />}
+      {error && <div>{error}</div>}
+      {!loading && !error && users.length > 0 && (
+        <table className="table table-striped table-hover">
+          <thead>
+            <tr>
+              <th>שם משתמש</th>
+              <th>מייל</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user._id}>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>
+                  <AlertDialog userId={user._id} renderUsers={fetchUsers} />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
 };
 
 export default AdminGetUsers;
--- src/hooks/useLogin.jsx
+++ src/hooks/useLogin.jsx
@@ -1,20 +1,23 @@
-import { useContext, useEffect, useState } from "react"
-import { UserContext } from "../App"
-import { useNavigate, useLocation } from "react-router-dom"
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "../App";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const useLogin = (path) => {
-    const { userInfo } = useContext(UserContext)
-    const navigate = useNavigate()
-    const location = useLocation()
+  const { userInfo } = useContext(UserContext);
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    useEffect(() => {
-        if (!userInfo) {
-            navigate(path || '/login')
-        } else if (userInfo.data.user.role !== 'admin' && location.pathname === "/admin") {
-            navigate('/')
-        }
-    }, [userInfo, path])
-    return userInfo
-}
+  useEffect(() => {
+    if (!userInfo) {
+      navigate(path || "/login");
+    } else if (
+      userInfo.data.user.role !== "admin" &&
+      location.pathname === "/admin"
+    ) {
+      navigate("/");
+    }
+  }, [userInfo, path]);
+  return userInfo;
+};
 
 export default useLogin;
