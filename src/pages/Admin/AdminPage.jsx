a3b3d9b8cd0df1387fd4675e39ca0a8d67159f5e
YishaiMic <frylich@gmail.com>
2024-06-02 11:58:31 +0300

    fixed add question

--- src/pages/Admin/AdminPage.jsx
+++ src/pages/Admin/AdminPage.jsx
@@ -1,43 +1,47 @@
-import React, { useState } from 'react';
-import useLogin from '../../hooks/useLogin';
-import AdminGetUsers from '../../features/AdminGetUsers'; 
-import AddQuestion from '../../features/AddQuestion';
+import React, { useState } from "react";
+import useLogin from "../../hooks/useLogin";
+import AdminGetUsers from "../../features/AdminGetUsers";
+import AddQuestion from "../../features/AddQuestion";
 
 const AdminPage = () => {
-    useLogin();
-    const [activeComponent, setActiveComponent] = useState(null);
+  useLogin();
+  const [activeComponent, setActiveComponent] = useState(null);
 
-    const handleToggleComponent = (component) => {
-        if (activeComponent === component) {
-            setActiveComponent(null);
-        } else {
-            setActiveComponent(component);
-        }
-    };
+  const handleToggleComponent = (component) => {
+    if (activeComponent === component) {
+      setActiveComponent(null);
+    } else {
+      setActiveComponent(component);
+    }
+  };
 
-    return (
-        <div>
-            <h1 className="m-3">דף ניהול</h1>
-            <nav className="nav">
-                <button
-                    id="but" className="btn btn-primary m-3"
-                    onClick={() => handleToggleComponent('AdminGetUsers')}
-                >
-                    {activeComponent === 'AdminGetUsers' ? 'הסתר משתמשים' : 'הצג משתמשים'}
-                </button>
-                <button
-                    id="but" className="btn btn-primary m-3"
-                    onClick={() => handleToggleComponent('AddQuestion')}
-                >
-                    {activeComponent === 'AddQuestion' ? 'הסתר טופס' : 'הוסף שאלה'}
-                </button>
-            </nav>
-            <div className="component-container">
-                {activeComponent === 'AdminGetUsers' && <AdminGetUsers showUsers={true} />}
-                {activeComponent === 'AddQuestion' && <AddQuestion showForm={true} />}
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="m-3">דף ניהול</h1>
+      <nav className="nav">
+        <button
+          id="but"
+          className="btn btn-primary m-3"
+          onClick={() => handleToggleComponent("AdminGetUsers")}
+        >
+          {activeComponent === "AdminGetUsers" ? "הסתר משתמשים" : "הצג משתמשים"}
+        </button>
+        <button
+          id="but"
+          className="btn btn-primary m-3"
+          onClick={() => handleToggleComponent("AddQuestion")}
+        >
+          {activeComponent === "AddQuestion" ? "הסתר טופס" : "הוסף שאלה"}
+        </button>
+      </nav>
+      <div className="component-container">
+        {activeComponent === "AdminGetUsers" && <AdminGetUsers />}
+        {activeComponent === "AddQuestion" && (
+          <AddQuestion setActiveComponent={setActiveComponent} />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default AdminPage;
