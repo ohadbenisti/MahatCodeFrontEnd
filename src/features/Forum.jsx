9c8a7e6eb486a5c1b585a3c9bd573e8488e92a7d
Sassi1000 <michaln250@gmail.com>
2024-06-02 17:47:20 +0300

    Forum Front

--- src/features/Forum.jsx
+++ src/features/Forum.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Comment from "./Comment";
 
 const Forum = () => {
-  return <div></div>;
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    const fetchComments = async () => {
+      try {
+        const response = await axios.get('/api/comments'); // יש להחליף את כתובת ה-API המתאימה
+        setComments(response.data.comments);
+      } catch (error) {
+        console.error("Error fetching comments:", error);
+      }
+    };
+
+    fetchComments();
+  }, []);
+
+  return (
+    <div>
+      <messageForm />
+      {comments.map((comment) => (
+        <Comment key={comment.id} comment={comment} />
+      ))}
+    </div>
+  );
 };
 
 export default Forum;
