9c37bbea77acf2c32f29b9779da09377ba53bd5c
Sassi1000 <michaln250@gmail.com>
2024-06-09 16:44:34 +0300

    adeddd

--- src/features/Header.jsx
+++ src/features/Header.jsx
@@ -1,43 +1,49 @@
 import { Link } from "react-router-dom";
 import useLogin from "../hooks/useLogin";
 import logo from "../assets/logo.jpg";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import LogoutIcon from '@mui/icons-material/Logout';
 import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
 import { Avatar } from "@mui/material";
 import "../pages/Content/Content.css";
-
 export const Header = ({ onLogout, isContentPage }) => {
   const userInfo = useLogin();
 
   return (
     <div className={`header-container ${isContentPage ? "header-transparent" : ""}`}>
-      <div className="header text-2xl w-full py-4">
+      <div className="header text-2xl w-full py-3">
         <div className="mx-auto flex justify-between items-center px-6">
           <div className="flex items-center">
-            <Link to="/" className="shrink-0">
-              <img src={logo} className="w-2/3 sm:w-1/2 max-w-[400px]" alt="logo" />
-            </Link>
-            {userInfo?.data?.user && (
-              <div className="flex items-center text-white ml-4">
-                <p className="text-white mr-2 md:mr-4">שלום {userInfo.data.user.name} 😊</p>
-                <Link to="/" className="text-white mr-2 md:mr-4">
-                  דף הבית
+            {userInfo ? (
+              <div className="flex items-center">
+                <Link to="/" className="shrink-0">
+                  <img src={logo} className="w-2/3 sm:w-1/2 max-w-[400px]" alt="logo" />
                 </Link>
               </div>
+            ) : (
+              <img src={logo} className="w-2/3 sm:w-1/2 max-w-[400px]" alt="logo" />
             )}
           </div>
           <div className="flex items-center sm:items-stretch gap-4 sm:gap-4">
             {userInfo?.data?.user && (
               <>
-                {userInfo?.data?.user?.role === "admin" && (
-                  <Link to="/admin">
-                    <ManageAccountsIcon style={{ color: '#FFD700' }} fontSize="large" />
-                  </Link>
+                {userInfo?.data?.user?.role === "admin" ? (
+                  <>
+                    <p className="text-white" style={{ marginLeft: '0px' }}>שלום {userInfo.data.user.name} </p>
+                    <Link to="/admin">
+                      <ManageAccountsIcon style={{ color: '#FFD700' }} fontSize="large" />
+                    </Link>
+                    <LogoutIcon onClick={onLogout} fontSize="large" style={{ marginTop: '3px', color: "white" }} />
+                  </>
+                ) : (
+                  <>
+                    <p className="text-white" style={{ marginLeft: '0px' }}>שלום {userInfo.data.user.name} </p>
+                    <Link to="/PersonalArea">
+                      <Avatar>{userInfo.data.user.name[0]}</Avatar>
+                    </Link>
+                    <LogoutIcon onClick={onLogout} fontSize="large" style={{ marginTop: '3px', color: "white" }} />
+                  </>
                 )}
-                <Link to="/PersonalArea">
-                  <Avatar>{userInfo.data.user.name[0]}</Avatar>
-                </Link>
-                <LogoutIcon onClick={onLogout} fontSize="large" style={{ marginTop: '3px', color: "white" }} />
               </>
             )}
           </div>
