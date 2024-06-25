f68e870bbc57aed479e1b298c251aee67db20853
YishaiMic <frylich@gmail.com>
2024-06-25 12:06:23 +0300

    banner

--- src/App.jsx
+++ src/App.jsx
@@ -32,20 +32,6 @@ function App() {
     navigate("/login");
   };
 
-  // const counter = () => {
-
-  //   const script = document.createElement("script");
-  //   script.type = "text/javascript";
-  //   script.src = "https://bringthemhomenow.net/1.1.0/hostages-ticker.js";
-  //   script.setAttribute(
-  //     "integrity",
-  //     "sha384-DHuakkmS4DXvIW79Ttuqjvl95NepBRwfVGx6bmqBJVVwqsosq8hROrydHItKdsne"
-  //   );
-  //   script.setAttribute("crossorigin", "anonymous");
-  //   document.getElementsByTagName("head")[0].appendChild(script);
-  // }
-  // counter()
-
   const isContentPage = location.pathname === "/";
 
   // React.useEffect(() => {
@@ -57,7 +43,6 @@ function App() {
   // }, []);
   return (
     <UserContext.Provider value={{ userInfo }}>
-      {/* <div id="bthn" lang="he"></div> */}
       <div className={isContentPage ? "content-background" : ""}>
         <Header onLogout={handleLogout} isContentPage={isContentPage} />
         <Routes>
@@ -69,7 +54,7 @@ function App() {
           <Route path="/problem/:questionId" element={<Problem />} />
           <Route path="/admin" element={<AdminPage />} />
           <Route path="/PersonalArea" element={<PersonalArea />} />
-          <Route path="/about" element={<About/>}/>
+          <Route path="/about" element={<About />} />
         </Routes>
         <Footer />
       </div>
--- src/pages/Content/Content.jsx
+++ src/pages/Content/Content.jsx
@@ -9,21 +9,35 @@ import "./Content.css"; // ייבוא של קובץ ה-CSS
 const Content = () => {
   const [filter, setFilter] = useState("");
   const userInfo = useLogin();
-
+  const counter = () => {
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "https://bringthemhomenow.net/1.1.0/hostages-ticker.js";
+    script.setAttribute(
+      "integrity",
+      "sha384-DHuakkmS4DXvIW79Ttuqjvl95NepBRwfVGx6bmqBJVVwqsosq8hROrydHItKdsne"
+    );
+    script.setAttribute("crossorigin", "anonymous");
+    document.getElementsByTagName("head")[0].appendChild(script);
+  };
+  counter();
   return (
-    <div className="content-background">
-      <div className="content-body">
-        <div className="container">
-          <TypeEffect />
-          <h2 className="title">תכניות לימוד</h2>
-          <CoursesComponent />
-          <h2 className="title"></h2>
-          <div className="my-4">
-            <FilteredQuestions setFilter={setFilter} />
+    <>
+      <div id="bthn" lang="he"></div>
+      <div className="content-background">
+        <div className="content-body">
+          <div className="container">
+            <TypeEffect />
+            <h2 className="title">תכניות לימוד</h2>
+            <CoursesComponent />
+            <h2 className="title"></h2>
+            <div className="my-4">
+              <FilteredQuestions setFilter={setFilter} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
